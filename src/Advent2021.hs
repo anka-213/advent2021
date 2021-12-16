b0702a8684b52ac27127a458783878bc6ec8fa4d
Andreas Källberg <anka.213@gmail.com>
2021-12-16 12:03:18 +0800

    Automatically download missing input files

--- src/Advent2021.hs
+++ src/Advent2021.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {- |
 Copyright: (c) 2021 Andreas Källberg
 SPDX-License-Identifier: MIT
@@ -14,22 +15,38 @@ module Advent2021
        , day2p2
        ) where
 
+import System.Environment (getArgs)
+import System.Directory
+import System.Process
 import Day1 (day1p1, day1p2)
 import Day2 (day2p1, day2p2)
-import System.Environment (getArgs)
+import Day3 ( day3p1, day3p2 )
+import Control.Monad (unless)
+import System.Exit ( ExitCode(ExitFailure, ExitSuccess) )
 
-
+-- | Run a day's solution
 runDay :: IO ()
 runDay = do
   [dayNr, partNr] <- map read <$> getArgs
   let solution = days !! (dayNr - 1) !! (partNr - 1)
   let inputFile = "inputs/day" ++ show dayNr ++ ".txt"
+  exists <- doesFileExist inputFile
+  unless exists $ downloadFile dayNr
   input <- readFile inputFile
   putStrLn $ "Day " ++ show dayNr ++ " part " ++ show partNr ++ ": " ++ solution input
   pure ()
 
+downloadFile :: Int -> IO ()
+downloadFile n = do
+  result <- rawSystem "./fetchInput.sh" [show n]
+  case  result of 
+    ExitSuccess -> pure ()
+    ExitFailure x -> error $ "Failed to download input file for day " ++ show n ++ " with exit code " ++ show x
+
+-- | All solutions for all days
 days :: [[String -> String]]
 days = 
   [[day1p1, day1p2]
   ,[day2p1, day2p2]
+  ,[day3p1, day3p2]
   ]
